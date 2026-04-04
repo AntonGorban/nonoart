753b9344689cd3adcdd9465016ec7d2597898691
Anton Gorban <antogor.work@gmail.com>
2026-04-04 13:19:44 +0300

    init server app

---
+++ server/src/main.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", async (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "hello world",
+  });
+});
+
+server.listen(7000, () => {
+  console.log("Server started on port 7000");
+});
---
+++ server/tsup.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/main.ts"],
+  format: ["esm"],
+  dts: false,
+  clean: true,
+  sourcemap: true,
+  bundle: true,
+  platform: "node",
+  target: "node18",
+});
