432f2388c8b466f3080cd808fec649a24403273d
Anton Gorban <antogor.work@gmail.com>
2026-04-05 16:45:43 +0300

    update server configs

--- server/tsup.config.js
+++ server/tsup.config.js
@@ -3,10 +3,15 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/main.ts'],
   format: ['esm'],
-  dts: false,
+  dts: true,
   clean: true,
   sourcemap: true,
   bundle: true,
   platform: 'node',
-  target: 'node18',
+  target: 'node20',
+  treeshake: true,
+  splitting: false,
+  minify: process.env.NODE_ENV === 'production',
+  tsconfig: './tsconfig.json',
+  external: ['express'],
 });
