a0e453cd97a7e809bd2cfa7f7e6ef40a7d2bb216
Anton Gorban <antogor.work@gmail.com>
2026-04-05 16:42:48 +0300

    update vite config

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
@@ -6,4 +8,34 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
+  resolve: {
+    conditions: ['import', 'module', 'browser', 'default'],
+    alias: {
+      '@nono-art/api': path.resolve(__dirname, '../libs/api/src'),
+      '@nono-art/api-types': path.resolve(__dirname, '../libs/api-types/src'),
+      '@nono-art/domain': path.resolve(__dirname, '../libs/domain/src'),
+      '@nono-art/types': path.resolve(__dirname, '../libs/types/src'),
+      '@nono-art/utils': path.resolve(__dirname, '../libs/utils/src'),
+    },
+  },
+  build: {
+    target: 'es2022',
+    minify: 'esbuild',
+    // minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules') && (id.includes('react') || id.includes('react-dom'))) {
+            return 'vendor';
+          }
+          // Можно добавить дополнительные чанки (например, для UI-библиотек)
+          // if (id.includes('@react-navigation')) return 'navigation';
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    open: false,
+  },
 });
