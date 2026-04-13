a774e0a1da148efbfd60a2c29d3bcc5f83c20aa4
Anton Gorban <antogor.work@gmail.com>
2026-04-13 12:19:19 +0300

    use logger for sql on server

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
 import { environment } from '../environment';
+import { context, logger } from '../services';
 
 import { Level, User } from './models';
 
@@ -12,5 +13,14 @@ export const db = new Sequelize({
   password: environment.PG_PASSWORD,
   database: environment.PG_DB,
   timezone: '+03:00',
+  logging: (sql: string, timing?: number) => {
+    const requestId = context.getRequestId();
+    logger.sql({
+      message: sql,
+      durationMs: timing,
+      requestId,
+      type: 'sql',
+    });
+  },
   models: [User, Level],
 });
