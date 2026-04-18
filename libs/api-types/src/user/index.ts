602bb64bd752280fb7009fe676e9e668e924dd79
Anton Gorban <antogor.work@gmail.com>
2026-04-18 20:57:07 +0300

    make REST.user.signOut

--- libs/api-types/src/user/index.ts
+++ libs/api-types/src/user/index.ts
@@ -1,4 +1,5 @@
 export * as get from './get.user.REST';
 export * as refreshToken from './refreshToken.user.REST';
 export * as signIn from './signIn.user.REST';
+export * as signOut from './signOut.user.REST';
 export * as signUp from './signUp.user.REST';
