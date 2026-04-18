6208c17ec7c60801d496477c1c0c04d700965144
Anton Gorban <antogor.work@gmail.com>
2026-04-18 15:27:38 +0300

    make user router on server

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
 import { levelRouter } from './level/level.router';
+import { userRouter } from './user/user.router';
 
 export const rootRouter = express();
 
 rootRouter.use('/level', levelRouter);
+rootRouter.use('/user', userRouter);
