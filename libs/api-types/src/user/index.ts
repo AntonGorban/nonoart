ac9f2c34fa59358641912e347da105a8d96f5c07
Anton Gorban <antogor.work@gmail.com>
2026-04-18 20:33:17 +0300

    make REST.user.refreshToken

--- libs/api-types/src/user/index.ts
+++ libs/api-types/src/user/index.ts
@@ -1,3 +1,4 @@
 export * as get from './get.user.REST';
+export * as refreshToken from './refreshToken.user.REST';
 export * as signIn from './signIn.user.REST';
 export * as signUp from './signUp.user.REST';
